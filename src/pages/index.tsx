356ef36d8b11f04755833e21072b226993500e39
tonyandy5630 <tonyandy456@gmail.com>
2023-09-26 04:49:13 +0700

    add comming soon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Container, LinearProgress, Stack, Typography } from '@mui/material'
+import React from 'react'
 
 export default function Index() {
-  return <div>Index</div>;
+    return (
+        <Container maxWidth="md" className="min-h-screen bg-black">
+            <Stack
+                justifyContent="center"
+                alignItems="center"
+                className="h-screen"
+                gap={5}
+            >
+                <Typography
+                    variant="h1"
+                    sx={{ color: '#FF8228', letterSpacing: '6px' }}
+                >
+                    APARTEE
+                </Typography>
+                <Typography className="text-white" variant="h2">
+                    COMMING SOON
+                </Typography>
+            </Stack>
+        </Container>
+    )
 }

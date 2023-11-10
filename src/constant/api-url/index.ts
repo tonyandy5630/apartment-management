3014cf75527ee342720b4175bb3737e977343bf0
tonyandy5630 <tonyandy456@gmail.com>
2023-11-11 00:44:54 +0700

    fix requests api

--- src/constant/api-url/index.ts
+++ src/constant/api-url/index.ts
@@ -1,5 +1,6 @@
 export const BASE_URL = `${process.env.NEXT_PUBLIC_AZURE_API}`
 
-export const REQUEST = `${BASE_URL}/request`
+export const REQUEST = `${BASE_URL}/requests`
 export const REQUEST_DETAIL = `${BASE_URL}/request-detail`
 export const AUTHENTICATION = `${BASE_URL}/authentication`
+export const STAFF = `${BASE_URL}/staff`
